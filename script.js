50a025348fd3dcda6db6f4f59e98087764384f5b
NotBB <notice76610@gmail.com>
2023-05-28 20:52:15 +0200

    finish

--- script.js
+++ script.js
@@ -1,75 +1,78 @@
-$(document).ready(function() {
-    const API_KEY = '34e37f5a58e47db00f85928579f900f8';
-    const API_URL = 'https://api.themoviedb.org/3/search/movie';
-    const DETAILS_URL = 'https://api.themoviedb.org/3/movie/';
-  
-    $('#search-button').click(function() {
-      const query = $('#search-input').val();
-      searchMovies(query);
+// Remplacez 'YOUR_API_KEY' par votre propre clé d'API TMDB
+const apiKey = '34e37f5a58e47db00f85928579f900f8';
+
+// Récupère les données des dernières sorties de films
+fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    const movies = data.results;
+
+    // Parcourt les films et crée les éléments HTML pour afficher les images
+    const movieContainer = document.getElementById('movieContainer');
+    movies.forEach(movie => {
+      const movieCard = document.createElement('div');
+      movieCard.classList.add('movieCard');
+
+      const movieImage = document.createElement('img');
+      movieImage.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+      movieImage.alt = movie.title;
+
+      movieCard.appendChild(movieImage);
+      movieContainer.appendChild(movieCard);
     });
-  
-    function searchMovies(query) {
-      const url = `${API_URL}?api_key=${API_KEY}&query=${encodeURIComponent(query)}`;
-  
-      $.ajax({
-        url: url,
-        method: 'GET',
-        success: function(response) {
-          displayResults(response.results);
-        },
-        error: function(error) {
-          console.log('Erreur de recherche :', error);
-        }
+  })
+  .catch(error => console.log(error));
+
+
+// Fonction de recherche de films
+function searchMovies(query) {
+  // Effectue l'appel à l'API TMDB pour rechercher les films correspondants
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`)
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+
+      // Efface les films actuellement affichés
+      const movieContainer = document.getElementById('movieContainer');
+      movieContainer.innerHTML = '';
+
+      // Parcourt les films correspondants et crée les éléments HTML pour afficher les images
+      movies.forEach(movie => {
+        const movieCard = document.createElement('div');
+        movieCard.classList.add('movieCard');
+
+        const movieImage = document.createElement('img');
+        movieImage.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+        movieImage.alt = movie.title;
+
+        movieCard.appendChild(movieImage);
+        movieContainer.appendChild(movieCard);
       });
-    }
-  
-    function displayResults(results) {
-      const resultsContainer = $('#results');
-      resultsContainer.empty();
-  
-      if (results.length === 0) {
-        resultsContainer.append('<p>Aucun résultat trouvé.</p>');
-      } else {
-        results.forEach(function(movie) {
-          const movieElement = $('<div class="movie"></div>');
-          movieElement.append(`<h2>${movie.title}</h2>`);
-          movieElement.append(`<img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}" data-id="${movie.id}" data-overview="${movie.overview}" data-release="${movie.release_date}">`);
-          resultsContainer.append(movieElement);
-        });
-  
-        $('.movie img').click(function() {
-          const title = $(this).attr('alt');
-          const overview = $(this).data('overview');
-          const releaseDate = $(this).data('release');
-          const movieId = $(this).data('id');
-          getMovieDetails(movieId, title, overview, releaseDate);
-        });
-      }
-    }
-  
-    function getMovieDetails(movieId, title, overview, releaseDate) {
-      const url = `${DETAILS_URL}${movieId}?api_key=${API_KEY}`;
-  
-      $.ajax({
-        url: url,
-        method: 'GET',
-        success: function(response) {
-          const rating = response.vote_average;
-          displayMovieDetails(title, overview, releaseDate, rating);
-        },
-        error: function(error) {
-          console.log('Erreur de récupération des détails du film :', error);
-        }
-      });
-    }
-  
-    function displayMovieDetails(title, overview, releaseDate, rating) {
-      const detailsContainer = $('#details');
-      detailsContainer.empty();
-      detailsContainer.append(`<h2>${title}</h2>`);
-      detailsContainer.append(`<p>${overview}</p>`);
-      detailsContainer.append(`<p>Date de sortie : ${releaseDate}</p>`);
-      detailsContainer.append(`<p>Note : ${rating}/10</p>`);
-    }
-  });
-  
+    })
+    .catch(error => console.log(error));
+}
+
+// Gestionnaire d'événement pour le bouton "Rechercher"
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', () => {
+  const searchInput = document.getElementById('searchInput');
+  const query = searchInput.value.trim();
+
+  if (query !== '') {
+    searchMovies(query);
+    searchInput.value = '';
+  }
+});
+
+// Gestionnaire d'événement pour la saisie en temps réel dans la zone de recherche
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('keyup', () => {
+  const query = searchInput.value.trim();
+
+  if (query !== '') {
+    searchMovies(query);
+  }
+});
+
+
+
